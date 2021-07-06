7e8fc58541fa0651b85c9a90252ba244ae59f963
Alena Bartosh <alena.mathematics@gmail.com>
2021-07-06 22:20:01 +0300

    🔨 install_zsh.sh: Add script for installing zsh

---
+++ scripts/install_zsh.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# stop script if any command fails
+set -e
+
+CURRENT_DIR="$(readlink -f "${BASH_SOURCE%/*}")"
+source "${CURRENT_DIR}/colors.sh"
+
+echo "${BOLD}${ORANGE} Make apt update...${NC}"
+sudo apt update
+
+echo "${BOLD}${ORANGE} Install zsh & make it as default shell...${NC}"
+
+sudo apt install -y zsh
+sudo chsh -s "$(which zsh)"
+
+echo "${BOLD}${ORANGE} Friendly reminder: ${NC} To use \"zsh\" as the default shell, you need to re-login"
+echo " Then try these commands: "echo \$SHELL"; "\$SHELL --version""
